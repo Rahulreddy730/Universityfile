2b4cee6ed0ac1fc1b97e234a3d12835cab2be35d
Rahulreddy730 <53223295+Rahulreddy730@users.noreply.github.com>
2020-03-01 15:57:41 +0000

    Delete dbo.Student.sql

--- dbo.Student.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Student] (
-    [SId]       INT          NOT NULL,
-    [LastName]  VARCHAR (50) NULL,
-    [FirstName] VARCHAR (50) NULL,
-    [Cources]   CHAR (10)    NULL,
-    PRIMARY KEY CLUSTERED ([SId] ASC)
-);
-
